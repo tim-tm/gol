f7a350eafb4c4b6f3d312f951034a9b57c5315ac
Tim Teichmann <teichmanntim@outlook.de>
2024-04-14 00:59:20 +0200

    fix logic error in if statement

--- src/main.c
+++ src/main.c
@@ -111,7 +111,7 @@ int sum_neighbors(int **grid, int x, int y) {
     int sum = 0;
     for (int i = -1; i <= 1; ++i) {
         for (int j = -1; j <= 1; ++j) {
-            if (i == x || j == y) continue;
+            if (i == x && j == y) continue;
             int col = (x+i+grid_cols)%grid_cols;
             int row = (y+j+grid_rows)%grid_rows;
             sum += grid[col][row];
